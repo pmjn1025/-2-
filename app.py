6ed001980687018d666d9a245b17832154096d4c
pmjn1025 <pmjn1025@naver.com>
2022-07-12 01:14:48 +0900

    jinjia를 통한 상세페이지 저장 및 삭제 만들기

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ import requests
 app = Flask(__name__)
 
 
-client = MongoClient('52.78.245.231', 27017, username="test", password="test")
-db = client.dbsparta_plus_week2
-
-
+client = MongoClient('mongodb+srv://test:sparta@cluster0.32bcc.mongodb.net/cluster0?retryWrites=true&w=majority')
+db =  client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
@@ -20,6 +18,7 @@ def main():
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
+    # GET방식의 ?status_give = new 할때 new의 키
     status_receive = request.args.get("status_give")
     # API에서 단어 뜻 찾아서 결과 보내기
     # 여기서 데이터를 받아서 html로 보낼 것을 서버에서 정리해준다 jinjia-SSR(서버사이드랜더링)
@@ -33,16 +32,23 @@ def detail(keyword):
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
     # 단어 저장하기
+    word_receive = request.form["word_give"]
+    definition_receive = request.form["definition_give"]
+    doc = {"word": word_receive, "definition":definition_receive}
+    db.words.insert_one(doc)
 
-
-
-    return jsonify({'result': 'success', 'msg': '단어 저장'})
+    # f-string을 사용하면 문자열 안에서 함수를 호출한 결과를 삽입할 수 도 있습니다.
+    return jsonify({'result': 'success', 'msg': f'단어{word_receive} 저장'})
 
 
 @app.route('/api/delete_word', methods=['POST'])
 def delete_word():
     # 단어 삭제하기
-    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+
+    word_receive = request.form["word_give"]
+    db.words.delete_one({"word": word_receive})
+
+    return jsonify({'result': 'success', 'msg': f'단어 {word_receive} 삭제'})
 
 
 if __name__ == '__main__':
