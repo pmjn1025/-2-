72194fe324d0e8078f33769b62938df677900669
pmjn1025 <pmjn1025@naver.com>
2022-07-12 00:19:09 +0900

    jinjia2 적용해서 단어목록 받아오기(예외처리 None, 아스키코드 깨지는 것)

--- app.py
+++ app.py
@@ -20,14 +20,22 @@ def main():
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
-
+    status_receive = request.args.get("status_give")
     # API에서 단어 뜻 찾아서 결과 보내기
-    return render_template("detail.html", word=keyword)
+    # 여기서 데이터를 받아서 html로 보낼 것을 서버에서 정리해준다 jinjia-SSR(서버사이드랜더링)
+    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token ff0ab0c5d7d41a250d6f369ac0334979358a469e"})
+    result = r.json()
+    print(result)
+    # 받어온 것을 템플릿으로 보낸다.
+    return render_template("detail.html", word=keyword, result=result, status=status_receive)
 
 
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
     # 단어 저장하기
+
+
+
     return jsonify({'result': 'success', 'msg': '단어 저장'})
 
 
